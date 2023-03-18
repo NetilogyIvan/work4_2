c5e381551dcb613388161a56df29d5b8d6757499
NetilogyIvan <inkornelyuk@gmail.com>
2023-03-19 00:09:33 +0600

    BmiService added

---
+++ src/BmiService.java
@@ -0,0 +1,8 @@
+public class BmiService {
+
+    public int calculate(int massKg, double heightMeter) {
+        double myBmi = massKg/(heightMeter*heightMeter);
+
+        return (int) myBmi;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       BmiService service = new BmiService();
+        int massKg = 98;
+        double heightMeter = 1.87;
+        int myBmi = service.calculate(massKg, heightMeter);
+
+        System.out.println("Мой индекс массы тела: " + myBmi);
     }
 }
