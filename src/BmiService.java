10c2bac9eddcdf6ba528344fafd456fef9a9ad3c
NetilogyIvan <inkornelyuk@gmail.com>
2023-03-22 19:46:59 +0600

    Reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
 
     public int calculate(int massKg, double heightMeter) {
-        double myBmi = massKg/(heightMeter*heightMeter);
+        double myBmi = massKg / (heightMeter * heightMeter);
 
         return (int) myBmi;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-       BmiService service = new BmiService();
-        int massKg = 98;
-        double heightMeter = 1.87;
+        BmiService service = new BmiService();
+        int massKg = 58;
+        double heightMeter = 1.64;
         int myBmi = service.calculate(massKg, heightMeter);
 
         System.out.println("Мой индекс массы тела: " + myBmi);
